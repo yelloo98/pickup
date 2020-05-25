62cb6f9b92faf5ea570d14ce55d9c89382372100
도채원 <winnie@enjoyworks.co.kr>
2020-05-25 14:57:27 +0900

    api test

--- app/Http/Controllers/Godo/GodoController.php
+++ app/Http/Controllers/Godo/GodoController.php
@@ -1,18 +1,28 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Godo;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use EnjoyWorks\core\ResultFunction;
 use Illuminate\Http\Request;
 
 class GodoController extends Controller
 {
+    use ResultFunction;
 
 	public function postJoin(Request $request)
 	{
 	    $model = new Customer();
-	    $model->log = $request;
-	    $model->save();
+	    try {
+            $model->log = json_encode($request->all());
+            $model->save();
+
+        } catch (\Exception $e) {
+            $model->log = json_decode($e->getMessage());
+            $model->save();
+            return $this->returnFailed('저장에 실패 헀습니다.');
+        }
+        return $this->returnSuccess();
 	}
 }
