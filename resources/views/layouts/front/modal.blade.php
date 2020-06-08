67d385609fcff760ca825c77155b3a4df8688595
도채원 <winnie@enjoyworks.co.kr>
2020-06-08 16:37:22 +0900

    모달수정

--- resources/views/layouts/front/modal.blade.php
+++ resources/views/layouts/front/modal.blade.php
@@ -1,8 +1,7 @@
-
-<div class="fullPopup-wrapper">
+<div id="storeSearch" class="fullPopup-wrapper">
     <div class="header-section">
         <p>매장검색</p>
-        <button>
+        <button class="full-close">
             <img src="/front/dist/img/icon_X_B.png" alt="">
         </button>
     </div>
@@ -16,21 +15,21 @@
             <div class="tabTarget active">
                 <ul>
                     <li>
-                        <select name="" id="">
+                        <select class="noneVal" name="" id="">
                             <option value="" disabled selected hidden>지역</option>
                             <option value="">서울시</option>
                         </select>
                         <img src="/front/dist/img/icon_select01.png" alt="">
                     </li>
                     <li>
-                        <select name="" id="">
+                        <select class="noneVal" name="" id="">
                             <option value="" disabled selected hidden>시/군/구</option>
                             <option value="">광진구</option>
                         </select>
                         <img src="/front/dist/img/icon_select01.png" alt="">
                     </li>
                     <li>
-                        <select name="" id="">
+                        <select class="noneVal" name="" id="">
                             <option value="" disabled selected hidden>읍/면/동</option>
                             <option value="">군자동</option>
                         </select>
@@ -39,12 +38,106 @@
                 </ul>
                 <div class="map-area">
                     <div class="etc-text">
-                        <p>주소를 선책하시면<br>이곳에 매장이 표시됩니다.</p>
+                        <p>주소를 선택하시면<br>이곳에 매장이 표시됩니다.</p>
                     </div>
                 </div>
             </div>
-            <div class="tabTarget"></div>
-            <div class="tabTarget"></div>
+            <div class="tabTarget ">
+                <ul>
+                    <li class="full-size">
+                        <select name="" id="">
+                            <option value="" selected>내주변 1km</option>
+                            <option value="">내주변 3km</option>
+                            <option value="">내주변 5km</option>
+                        </select>
+                        <img src="/front/dist/img/icon_select01.png" alt="">
+                    </li>
+                </ul>
+                <div class="map-area fake-box">
+                    <div class="research-box">
+                        <p>여기에서 재검색</p>
+                        <img src="/front/dist/img/icon_refresh.png" alt="">
+                    </div>
+                    <div class="pin-wrap">
+                        <button class="pin default-pin">
+                            <img src="/front/dist/img/icon_pin.png" alt="">
+                        </button>
+                        <button class="pin focus-pin">
+                            <img src="/front/dist/img/icon_mainpin.png" alt="">
+                        </button>
+                    </div>
+                </div>
+                <div class="storeInfo-slide">
+                    <div class="store-header">
+                        <span>군자점</span>
+                        <button>매장선택</button>
+                    </div>
+                    <div class="store-address">
+                        <p>경상남도 경기도 성남시 중현구 양원2로 11번길 21345호21345호</p>
+                        <small>02)234-1234</small>
+                    </div>
+                </div>
+            </div>
+            <div class="tabTarget ">
+                <div class="enjoyStore-list">
+                    <ul>
+                        <li>
+                            <div class="store-info">
+                                <div class="store-header">
+                                    <span>군자점</span>
+                                </div>
+                                <div class="store-address">
+                                    <p>경상남도 경기도 성남시 중현구 양원2로 11번길 21345호21345호</p>
+                                    <small>02)234-1234</small>
+                                </div>
+                            </div>
+                            <img src="/front/dist/img/icon_arrow_MR.png" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+    </div>
+</div>
+
+
+<div id="PPalarm" class="fullPopup-wrapper">
+    <div class="header-section">
+        <p>알림</p>
+        <button class="full-close">
+            <img src="img/icon_X_B.png" alt="">
+        </button>
+    </div>
+    <div class="list-section alarm-list">
+        <ul>
+            <li>
+                <button class="delete-list">
+                    <img src="img/icon_x_S.png" alt="">
+                </button>
+                <div class="list-date">
+                    <p>2020-03-22</p>
+                </div>
+                <div class="list-subject">
+                    <p>삼겹살이 2000원!!</p>
+                </div>
+                <div class="list-content">
+                    <p>오직 프래쉬스토어 성남점에서만 볼 수 있는 이벤트를 진행합니다! 삼겹살이 2000원! 오직 프래쉬스토어 성남점에서만 볼 수 있는 이벤트를 진행합니다! 삼겹살이 2000원!</p>
+                </div>
+            </li>
+            <li>
+                <button class="delete-list">
+                    <img src="img/icon_x_S.png" alt="">
+                </button>
+                <div class="list-date">
+                    <p>2020-03-22</p>
+                </div>
+                <div class="list-subject">
+                    <p>삼겹살이 2000원!!</p>
+                </div>
+                <div class="list-content">
+                    <p>오직 프래쉬스토어 성남점에서만 볼 수 있는 이벤트를 진행합니다! 삼겹살이 2000원! 오직 프래쉬스토어 성남점에서만 볼 수 있는 이벤트를 진행합니다! 삼겹살이 2000원!</p>
+                </div>
+            </li>
+        </ul>
     </div>
 </div>
