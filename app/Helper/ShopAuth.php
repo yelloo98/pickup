be8a4f9539900216fdd28e85e53fff672064e61a
sungjin <yelloo98@enjoyworks.co.kr>
2020-07-23 17:46:04 +0900

    20200723 - 로그인 수정

--- app/Helper/ShopAuth.php
+++ app/Helper/ShopAuth.php
@@ -15,23 +15,22 @@ class ShopAuth
 
     public function __construct(Request $request) {
         $this->user = Customer::find(1452);
-        /*
-                try {
-        //            $sessVal = $request->cookie(env("SESS_COOKIE_NAME", "GD5SESSID"));    // not work ?? return null
-                    $sessVal = $_COOKIE[env("SESS_COOKIE_NAME", "GD5SESSID")];
-                    if($sessVal == null)  $sessVal = env("SESS_COOKIE_VALUE", "m07mjcpvm61gqk6h0din7ah33pb4s88vv3rm4hhbsi93a9e5iqpblpc9pl8so99auneibs2oftj6bh3odiogjm9k0q94ciro45619v1");
-                    $http = new Client(['cookies' => true]);
-                    $cookieJar = CookieJar::fromArray([
-                        env("SESS_COOKIE_NAME", "GD5SESSID") => $sessVal
-                    ], env("SESSION_DOMAIN", ".chaesukwoo1.godomall.com"));
-                    $res = $http->get(env("SHOP_URL", "http://m.chaesukwoo1.godomall.com/")."member/memberCheck.php", ['cookies' => $cookieJar, 'debug' => false]);
-                    $body = json_decode($res->getBody()->getContents());
-                    if($body->res->code == 200 && $body->data != null) {
-                        $this->user = Customer::where('memNo', $body->data->memNo)->first();
-                    }
-                } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-                    return response()->json(['code' => 400, 'msg' => '쇼핑몰 연동 에러']);
-                }*/
+        try {
+//            $sessVal = $request->cookie(env("SESS_COOKIE_NAME", "GD5SESSID"));    // not work ?? return null
+            $sessVal = $_COOKIE[env("SESS_COOKIE_NAME", "GD5SESSID")];
+            if($sessVal == null)  $sessVal = env("SESS_COOKIE_VALUE", "m07mjcpvm61gqk6h0din7ah33pb4s88vv3rm4hhbsi93a9e5iqpblpc9pl8so99auneibs2oftj6bh3odiogjm9k0q94ciro45619v1");
+            $http = new Client(['cookies' => true]);
+            $cookieJar = CookieJar::fromArray([
+                env("SESS_COOKIE_NAME", "GD5SESSID") => $sessVal
+            ], env("SESSION_DOMAIN", ".chaesukwoo1.godomall.com"));
+            $res = $http->get(env("SHOP_URL", "http://m.chaesukwoo1.godomall.com/")."member/memberCheck.php", ['cookies' => $cookieJar, 'debug' => false]);
+            $body = json_decode($res->getBody()->getContents());
+            if($body->res->code == 200 && $body->data != null) {
+                $this->user = Customer::where('memNo', $body->data->memNo)->first();
+            }
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            return response()->json(['code' => 400, 'msg' => '쇼핑몰 연동 에러']);
+        }
     }
 
     // return user
